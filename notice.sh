556e0b6429ed309cf2032d9595a12f95ca95094c
Sets3n <76554585+Sets3n@users.noreply.github.com>
2022-12-14 21:09:36 +0800

    Update notice.sh

--- notice.sh
+++ notice.sh
@@ -1,5 +1,5 @@
 #！/bin/bash
 echo $1
-curl "https://oapi.dingtalk.com/robot/send?access_token=$1" \
+curl -s "https://oapi.dingtalk.com/robot/send?access_token=$1" \
                 -H 'Content-Type: application/json' \
                 -d '{"msgtype": "text","text": {"content":"已经暂停"}}'
